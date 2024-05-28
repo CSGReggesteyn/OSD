26f7e69189a1d2610b8df0d18af6cb1ecf28f681
CSGReggesteyn <helpdesk@reggesteyn.nl>
2024-05-28 11:43:48 +0200

    Update OSDCloudStartURL.ps1

--- OSDCloudStartURL.ps1
+++ OSDCloudStartURL.ps1
@@ -17,10 +17,7 @@ $MainMenu = {
     Write-Host " ***************************"
     Write-Host
     Write-Host " 1.) OSDCloud Local (WinPE)"
-    Write-Host " 2.) OSDCloud Azure (WinPE)"
-    Write-Host " 3.) OSDCloud Azure Sandbox (WinPE)"
-    Write-Host " 4.) Install/Update OSDCloudUSB (Windows)"
-    Write-Host " 5.) Autopilot (Windows)"
+    Write-Host " 2.) Install/Update OSDCloudUSB (Windows)"
     Write-Host " Q.) Exit Menu"
     Write-Host
     Write-Host " Select an option and press Enter: "  -nonewline
@@ -36,29 +33,7 @@ Do {
             Start-OSDCloudGUI
         }
         2 {
-            Start-OSDCloudAzure
-        }
-        3 {
-            iex (irm az.osdcloud.com)
-        }
-        4 {
             Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudUpdateMenu.ps1'
-        }
-        5 {
-            Clear-Host
-            Write-Host " ***************************"
-            Write-Host " *         Autopilot       *"
-            Write-Host " ***************************"
-            Write-Host
-            Write-Host " A login window will open, login with an Autopilot authorized user account"
-            Write-Host
-            try {
-                Get-WindowsAutopilotInfo.ps1 -online
-            } Catch {
-                Write-Host "Autopilot script not found, installing script"
-                install-script -Name Get-WindowsAutoPilotInfo -Force
-                Get-WindowsAutopilotInfo.ps1 -online
-            }
         }
         Q {
             Exit
