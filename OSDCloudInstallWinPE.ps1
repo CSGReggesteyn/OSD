721b50e6df6f5982a7ebc11593ce7d79dcf23797
CSGReggesteyn <helpdesk@reggesteyn.nl>
2024-06-07 09:07:00 +0200

    Update OSDCloudInstallWinPE.ps1

--- OSDCloudInstallWinPE.ps1
+++ OSDCloudInstallWinPE.ps1
@@ -1,62 +1,48 @@
 Clear-Host
 Write-Host " ***************************"
-Write-Host " *    WinPE Installatie    *"
+Write-Host " *  OSDCloud Installation  *"
 Write-Host " ***************************"
 Write-Host
 
 ### Test if any USB drive is fount
-$testdisk = GET-WMIOBJECT win32_diskdrive | Where { $_.mediatype -eq 'Removable Media' -or $_.mediatype -eq 'Removable Media' -or $_.InterfaceType -eq 'USB' }
+$testdisk = GET-WMIOBJECT win32_diskdrive | Where-Object { $_.mediatype -eq 'Removable Media' -or $_.mediatype -eq 'Removable Media' -or $_.InterfaceType -eq 'USB' }
 #Option 2: $testdisk2 = Get-Disk | Where-Object -FilterScript {$_.Bustype -Eq "USB"}
 if ( $testdisk -eq $null) {
-    Write-Host " Geen USB Stick gevonden"
+    Write-Host "No USB Drive found, going back to menu"
     Write-Host
     cmd /c 'pause'
-    Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudUpdateMenu.ps1'   
+    Invoke-WebPSScript "$RepositoryURL/OSDCloudUpdateMenu.ps1"
 } else {
-    Write-Host " WinPE aan het downloaden"
+    Write-Host " Downloading WinPE"
     Write-Host
     
     ### Starting WinPE install from Azure Blob and writing the necessary files
-    New-OSDCloudUSB -fromIsoUrl 'https://osdcloudreg.blob.core.windows.net/osdcloudiso/OSDCloud_NoPrompt.iso'
+    New-OSDCloudUSB -fromIsoUrl "$ISOURL"
     
+    ### Re-enter Variables that where set in OSDCloudUpdateMenu.ps1
     ### Search OSDCloud disk after installation
     $disk = Get-WMIObject Win32_Volume | Where-Object { $_.Label -eq 'OSDCloudUSB' }
     $disk = $disk.Name
     
-    ### Getting version from .\Update\Version.txt and .\Update\VersionWinPE.txt
-    $version = Invoke-WebRequest -Uri https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/Update/Version.txt
-    $version = $version.Content.Split([Environment]::NewLine) | Select -First 1
-    $versionWinPE = Invoke-WebRequest -Uri https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/Update/VersionWinPE.txt
-    $versionWinPE = $versionWinPE.Content.Split([Environment]::NewLine) | Select -First 1
-    
-    ### Getting OSDCloudUSB and WinPE version from drive
-    $file = "Version.txt"
-    $fileWinPE = "VersionWinPE.txt"
-    $folder = 'OSDCloud\'
-    $location = "$disk$folder"
-    $versionondisk = Get-Content "$location$file" -ErrorAction SilentlyContinue
-    $versionWinPEondisk = Get-Content "$location$fileWinPE" -ErrorAction SilentlyContinue
-    
     ### Creating files on de OSDCloud drive
     New-Item -ItemType Directory -Path $location\Automate -force -ErrorAction SilentlyContinue | Out-Null
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/Update/Automate/Start-OSDCloudGUI.json -OutFile $location\Automate\Start-OSDCloudGUI.json
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/Update/Start-Menu.ps1 -OutFile $location\Start-Menu.ps1
+    Invoke-WebRequest -Uri $RepositoryURL/Update/Automate/Start-OSDCloudGUI.json -OutFile $location\Automate\Start-OSDCloudGUI.json
     New-Item -Path "$location" -Name "$file" -ItemType "file" -Value $version -force -ErrorAction SilentlyContinue | Out-Null
     New-Item -Path "$location" -Name "$fileWinPE" -ItemType "file" -Value $versionWinPE -force -ErrorAction SilentlyContinue | Out-Null
     
     $MainMenu = {
         Write-Host " ***************************"
-        Write-Host " *    WinPE Installation   *"
+        Write-Host " *  OSDCloud Installation  *"
         Write-Host " ***************************"
         Write-Host
-        Write-Host " WinPE is geinstalleerd"
-        Write-Host " Wil je nog een USB Stick klaarmaken?"
+        Write-Host " WinPE Installation complete"
+        Write-Host " Install one more WinPE to a USB drive?"
         Write-Host
-        Write-Host " 1.) Ja (Plug een nieuwe USB Stick in en ga dan pas door)"
-        Write-Host " 2.) Nee"
-        Write-Host " Q.) Terug (Dit bewaard de ISO in de Downloads folder (Niet geadviseerd))"
+        Write-Host " 1.) Yes (insert new USB drive before continuing)"
+        Write-Host " 2.) No (delete downloaded ISO)"
+        Write-Host " Q.) Back (keeps ISO in Download folder)"
         Write-Host
-        Write-Host " Selecteer een optie en druk op Enter: "  -nonewline
+        Write-Host " Select an option and press Enter: "  -nonewline
     }
     Clear-Host
     Do {
@@ -66,17 +52,16 @@ if ( $testdisk -eq $null) {
         Switch ($Select)
             {
             1 {
-                Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudInstallWinPE.ps1'
+                Invoke-WebPSScript $RepositoryURL/OSDCloudInstallWinPE.ps1
             }
             2 {
                 ### Getting the active download folder, Dismounting the image (if mounted) and removing the ISO file
-                $ISOPath = (New-Object -ComObject Shell.Application).Namespace('shell:Downloads').Self.Path
-                Dismount-DiskImage -ImagePath "$ISOPath\OSDCloud_NoPrompt.iso" -ErrorAction SilentlyContinue | Out-Null
-                Remove-Item $ISOPath\OSDCloud_NoPrompt.iso -ErrorAction SilentlyContinue | Out-Null
-                Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudUpdateMenu.ps1'
+                Dismount-DiskImage -ImagePath "$DownloadsPath\OSDCloud_NoPrompt.iso" -ErrorAction SilentlyContinue | Out-Null
+                Remove-Item $DownloadsPath\OSDCloud_NoPrompt.iso -ErrorAction SilentlyContinue | Out-Null
+                Invoke-WebPSScript $RepositoryURL/OSDCloudUpdateMenu.ps1
             }
             Q {
-                Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudUpdateMenu.ps1'
+                Invoke-WebPSScript $RepositoryURL/OSDCloudUpdateMenu.ps1
             }
         }       
     }
