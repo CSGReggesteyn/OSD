74a18cbe302c37cf8113e542b4fc80cc63d622d9
CSGReggesteyn <helpdesk@reggesteyn.nl>
2024-06-07 09:06:49 +0200

    Update OSDCloudDownloadWindows.ps1

--- OSDCloudDownloadWindows.ps1
+++ OSDCloudDownloadWindows.ps1
@@ -1,15 +1,15 @@
 ### Getting Windows versions and language opties from "Start-OSDCloudGUI.json"
-$json = Invoke-WebRequest 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/Update/Automate/Start-OSDCloudGUI.json' | ConvertFrom-Json
-$WinVer = $json | ForEach-Object { $_.OSNameValues }
-$winVer = $WinVer.Replace(" x64", "")
-$WinLang = $json | ForEach-Object { $_.OSLanguageValues }
+$json     = Invoke-WebRequest $RepositoryURL/Update/Automate/Start-OSDCloudGUI.json | ConvertFrom-Json
+$WinVer   = $json | ForEach-Object { $_.OSNameValues }
+$winVer   = $WinVer.Replace(" x64", "")
+$WinLang  = $json | ForEach-Object { $_.OSLanguageValues }
 
 Clear-Host
 Write-Host " ***************************"
 Write-Host " * OSDCloud Offline Windows*"
 Write-Host " ***************************"
 Write-Host
-Write-Host " Selecteer een Windows versie:"
+Write-Host " Select a Windows version:"
 
 ### Listing Windows versions
 $MenuVer = @{}
@@ -18,20 +18,19 @@ $WinVer | ForEach-Object -Begin {$i = 1} {
      $MenuVer.add("$i",$_)
      $i++
 }
-
-Write-Host " Q.) Terug"
-
-$WinVerSelection = Read-Host " Selecteer een optie en druk op Enter:"
+Write-Host " Q.) Back"
+Write-Host
+$WinVerSelection = Read-Host " Select an option and press Enter: "
 
 if ($WinVerSelection -eq 'Q') { 
-     Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudUpdateMenu.ps1' 
+     Invoke-WebPSScript $RepositoryURL/OSDCloudUpdateMenu.ps1
 } Else { 
 Clear-Host
 Write-Host " ***************************"
 Write-Host " * OSDCloud Offline Windows*"
 Write-Host " ***************************"
 Write-Host
-Write-Host " Selecteer een Taal:"
+Write-Host " Select a Windows language: "
 
 ### Listing language opties
 $MenuLang = @{}
@@ -40,13 +39,12 @@ $WinLang | ForEach-Object -Begin {$i = 1} {
      $MenuLang.add("$i",$_)
      $i++
 }
-
-Write-Host " Q.) Terug"
-
-$WinLangSelection = Read-Host " Selecteer een optie en druk op Enter:"
+Write-Host " Q.) Back"
+Write-Host
+$WinLangSelection = Read-Host " Select an option and press Enter: "
 
 if ($WinLangSelection -eq 'Q') { 
-     Invoke-WebPSScript 'https://raw.githubusercontent.com/CSGReggesteyn/OSD/main/OSDCloudUpdateMenu.ps1' 
+     Invoke-WebPSScript $RepositoryURL/OSDCloudUpdateMenu.ps1
 } Else { 
      Clear-Host
      Update-OSDCloudUSB -OSName $MenuVer.$WinVerSelection -OSLanguage $menuLang.$WinLangSelection -OSLicense Retail
